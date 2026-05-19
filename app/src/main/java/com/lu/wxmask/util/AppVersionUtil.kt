82d70e65312b90e454c2eb0d55e33afb70311462
seardna <seardna.summer@gmail.com>
2026-05-19 09:58:00 +0800

    Update AppVersionUtil.kt

--- app/src/main/java/com/lu/wxmask/util/AppVersionUtil.kt
+++ app/src/main/java/com/lu/wxmask/util/AppVersionUtil.kt
@@ -69,7 +69,8 @@ class AppVersionUtil {
                 Constrant.WX_CODE_8_0_51,
                 Constrant.WX_CODE_8_0_53,
                 Constrant.WX_CODE_8_0_56,
-                Constrant.WX_CODE_8_0_58
+                Constrant.WX_CODE_8_0_58,
+                Constrant.WX_CODE_PLAY_8_0_69
                 -> true
 
                 else -> false
